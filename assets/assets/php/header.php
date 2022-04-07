99137d82f649067f29a2b99e66f38da5ddd33b30
JuniorXcoder <mrjunior403@gmail.com>
2022-04-07 17:44:57 +0700

    change template & worked login
    - template assets telah dirubah menjadi template premi from sb2 admin
    - worked admin

--- assets/assets/php/header.php
+++ assets/assets/php/header.php
@@ -1,5 +1,3 @@
-<!-- BEGIN: Head-->
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,13 +5,13 @@
     <meta name="description" content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
     <meta name="keywords" content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
     <meta name="author" content="PIXINVENT">
-    <title><?php echo $title ?></title>
+    <title>Login Page - Modern Admin - Clean Bootstrap 4 Dashboard HTML Template + Bitcoin Dashboard</title>
     <link rel="apple-touch-icon" href="../assets/app-assets/images/ico/apple-icon-120.png">
     <link rel="shortcut icon" type="image/x-icon" href="../assets/app-assets/images/ico/favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="../assets/app-assetsvendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/app-assets/vendors/css/vendors.min.css">
     <link rel="stylesheet" type="text/css" href="../assets/app-assets/vendors/css/forms/icheck/icheck.css">
     <link rel="stylesheet" type="text/css" href="../assets/app-assets/vendors/css/forms/icheck/custom.css">
     <!-- END: Vendor CSS-->
@@ -29,15 +27,11 @@
     <link rel="stylesheet" type="text/css" href="../assets/app-assets/css/core/menu/menu-types/vertical-menu.css">
     <link rel="stylesheet" type="text/css" href="../assets/app-assets/css/core/colors/palette-gradient.css">
     <link rel="stylesheet" type="text/css" href="../assets/app-assets/css/pages/login-register.css">
-    <link rel="stylesheet" type="text/css" href="../assets/app-assets/css/core/colors/palette-callout.css">
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
     <link rel="stylesheet" type="text/css" href="../assets/assets/css/style.css">
     <!-- END: Custom CSS-->
 
-
-
-    
 </head>
 <!-- END: Head-->
